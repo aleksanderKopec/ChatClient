3a8cd9179dbe9f0a538532c2982278c4aea0f197
aleksanderKopec <a.kopec.b@gmail.com>
2021-10-02 23:48:20 +0200

    Added asynchronous receiving and sending of messages
    Clients can now asynchronously send and receive messages from other clients through the server.

--- ChatClient/Client.cs
+++ ChatClient/Client.cs
@@ -11,85 +11,167 @@ namespace ChatClient
     public class Client
     {
         private string Name { get; set; }
-        private IPAddress ServerIP { get; set; }
-        private IPEndPoint EndPoint { get; set; }
-        private Socket socket;
-        public Client(string serverIP, string port, string name = "Klient1")
+
+        private Socket connection { get; set; }
+        public Client(string serverIP, int port, string name = "Anonymous")
         {   
             //Resolve the connection information
             IPHostEntry ipHost = Dns.GetHostEntry(serverIP);
-            this.ServerIP = ipHost.AddressList[0];
-            this.EndPoint = new IPEndPoint(this.ServerIP, 12345);
+            IPAddress ip = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
             this.Name = name;
 
             //Connect to server and start communication
-            createNewSocket();
-            connectToServer();
+            connectToServer(ip);
+            startCommunication(endPoint);
+
         }
 
-        private void createNewSocket()
+        private void connectToServer(IPAddress serverIP)
         {
             try
             {
-                this.socket = new Socket(this.ServerIP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                this.connection = new Socket(serverIP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine($"Caught SocketException: {se}");
+                Console.WriteLine("Failed to connect to server, terminating client");
+            }
+            
+        }
+
+        private void startCommunication(IPEndPoint endPoint)
+        {
+            try
+            {
+                //Connecting to server
+                connection.Connect(endPoint);
+                Console.WriteLine($"Connection with {endPoint} established");
+
+                //Starting the task to receive messages
+                Task.Run(() => handleReceivedMessages());
+
+                //Sending messages
+                while (true)
+                {
+                    //Console.Write($"{this.Name}: ");
+                    string decodedMessage = Console.ReadLine();
+                    ClearLine();
+                    //Console.Write($"\n");
+
+                    byte[] encodedMessage = Encoding.ASCII.GetBytes(decodedMessage);
+                    connection.Send(encodedMessage);
+                    //Console.WriteLine($"{this.Name}: {decodedMessage}");
+                }
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine($"Caught SocketException: {se}");
+                Console.WriteLine($"Connection terminated, killing client...");
+                return;
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                Console.WriteLine($"Caught unhandled exception: {e}");
+                Console.WriteLine($"Killing client...");
             }
+
+
         }
 
-        private void connectToServer()
+        private void handleReceivedMessages()
         {
+            byte[] encodedMessage = new byte[1024];
             try
             {
-                //Establish connection
-                socket.Connect(this.EndPoint);
-                Console.WriteLine($"Connection with {this.EndPoint} established");
+                while (true)
+                {
+                    connection.Receive(encodedMessage);
 
-                //Send a message
-                string msg = "This is a test";
-                sendMsg(msg);
-
-                //Wait for response
-                handleIncomingMessage();
+                    string decodedMessage = Encoding.ASCII.GetString(encodedMessage);
+                    Console.WriteLine($"Anonymous: {decodedMessage}");
+                }
             }
-            catch (Exception e)
+            catch (SocketException se)
             {
-                Console.WriteLine(e);
+                Console.WriteLine($"Caught SocketException {se}");
+                Console.WriteLine($"Connection timed out, killing client...");
+                return;
             }
+            
         }
 
-        private void handleIncomingMessage()
+        public static void ClearLine()
         {
-            try
-            {
-                //Receive encoded data from server;
-                byte[] encodedMsg = new byte[1024];
-                this.socket.Receive(encodedMsg);
-
-                //Decode data
-                string msg = Encoding.ASCII.GetString(encodedMsg);
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            Console.SetCursorPosition(0, Console.CursorTop-1);
+            Console.Write(new string(' ', Console.BufferWidth-1));
+            Console.SetCursorPosition(0, Console.CursorTop);
         }
 
-        private void sendMsg(string msg)
-        {
-            try
-            {
-                //Encode data
-                byte[] encodedMsg = Encoding.ASCII.GetBytes(msg);
-                this.socket.Send(encodedMsg);
-                Console.WriteLine($"{this.Name}: {msg}");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-        }
+        //private void createNewSocket()
+        //{
+        //    try
+        //    {
+        //        this.socket = new Socket(this.ServerIP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e);
+        //    }
+        //}
+
+        //private void connectToServer()
+        //{
+        //    try
+        //    {
+        //        //Establish connection
+        //        socket.Connect(this.EndPoint);
+        //        Console.WriteLine($"Connection with {this.EndPoint} established");
+
+        //        //Send a message
+        //        string msg = "This is a test<EOF>";
+        //        sendMsg(msg);
+
+        //        //Wait for response
+        //        handleIncomingMessage();
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e);
+        //    }
+        //}
+
+        //private void handleIncomingMessage()
+        //{
+        //    try
+        //    {
+        //        //Receive encoded data from server;
+        //        byte[] encodedMsg = new byte[1024];
+        //        this.socket.Receive(encodedMsg);
+
+        //        //Decode data
+        //        string msg = Encoding.ASCII.GetString(encodedMsg);
+        //    }
+        //    catch(Exception e)
+        //    {
+        //        Console.WriteLine(e);
+        //    }
+        //}
+
+        //private void sendMsg(string msg)
+        //{
+        //    try
+        //    {
+        //        //Encode data
+        //        byte[] encodedMsg = Encoding.ASCII.GetBytes(msg);
+        //        this.socket.Send(encodedMsg);
+        //        Console.WriteLine($"{this.Name}: {msg}");
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e);
+        //    }
+        //}
     }
 }
--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -6,7 +6,7 @@ namespace ChatClient
     {
         static void Main(string[] args)
         {
-            var client = new Client("127.0.0.1", "12345");
+            var client = new Client("127.0.0.1", 12345);
         }
     }
 }
